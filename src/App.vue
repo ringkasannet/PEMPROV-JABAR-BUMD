56b382b10131ae447b6b1af5f5c6fd62ef5e264e
begjooo <begjooo@gmail.com>
2024-05-07 17:40:26 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,34 @@
       <div id="chat_container">
         <UserInputChat />
       </div>
-      <div id="rekomendasi_container">
-        <DaftarRekomendasi />
-      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-
-import HeaderContainer from './components/HeaderContainer.vue'
+import HeaderContainer from './components/HeaderContainer.vue';
 import UserInputChat from './components/UserInputChat.vue';
-import DaftarRekomendasi from './components/DaftarRekomendasi.vue'
-
 </script>
 
 <style scoped>
-
-#outer_container{
-  width: 100vw;
+#outer_container {
+  width: 1000%;
+  max-width: 1000px;
   display: flex;
   justify-content: center;
+  position: relative;
 }
 
-#main_container{
-  width: 100vw;
-  max-width: 1000px;
+#main_container {
+  width: 100%;
 }
 
 #chat_container {
   margin-top: 50px;
 }
 
+#answer_container {
+  background: red;
+  width: 1000px;
+}
 </style>
